9ae3d3a7d1183b17a32adf718f4db5b1948955e3
chruni <chruni@eltiempo.com>
2017-11-02 10:19:12 -0500

    Added react user data component to render user information.

--- src/app.js
+++ src/app.js
@@ -7,14 +7,37 @@ import ReactDOM from 'react-dom';
 
 // Dev components
 import userService from './js/userService';
+import UserData from './js/userDataComponent';
 
+let dataUserContainer = document.querySelector('.js-data-container');
 let getUserData = userService({
   method: 'GET',
   action: '/users'
 });
 
+class UserList extends React.Component {
+  render() {
+    let {list} = this.props;
+    return(
+      list.map(item => {
+        return (
+          <UserData
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            username={item.username}
+            email={item.email} />
+        )
+      })
+    )
+  }
+}
+
 getUserData.then((data) => {
   let dataJson = JSON.parse(data);
 
-  console.log(dataJson);
+  ReactDOM.render(
+    <UserList list={dataJson}/>,
+    dataUserContainer
+  );
 });
